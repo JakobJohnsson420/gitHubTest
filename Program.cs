f2bf8b3b649afe958cd1032b97b47c629f78428a
JakobJohnsson420 <jakobkappason@gmail.com>
2017-01-19 09:42:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            writeClass writeclass = new writeClass();
+
+
+            writeclass.Write();
+        }
+    }
+}
---
+++ writeClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitHubTest
+{
+    class writeClass
+    {
+       public void Write()
+        {
+            Console.WriteLine("Hello    World!");
+            Console.Read();
+        }
+        
+    }
+}
